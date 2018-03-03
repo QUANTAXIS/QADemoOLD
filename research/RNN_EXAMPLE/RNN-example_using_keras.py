6860794fe6bead007dff783c3e9d134c1b5fcb38
yutiansut <yutiansut@qq.com>
2018-03-03 20:11:16 +0800

    update

--- research/RNN_EXAMPLE/RNN-example_using_keras.py
+++ research/RNN_EXAMPLE/RNN-example_using_keras.py
@@ -9,7 +9,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from pandas import datetime
-import math, time
+import math
+import time
 import itertools
 from sklearn import preprocessing
 import datetime
@@ -25,11 +26,11 @@ import QUANTAXIS as QA
 # In[23]:
 
 
-## load data function
+# load data function
 def load_data(stock, seq_len):
     # stock 是一个input的dataframe
     amount_of_features = len(stock.columns)
-    data = stock.as_matrix() #pd.DataFrame(stock)
+    data = stock.as_matrix()  # pd.DataFrame(stock)
     sequence_length = seq_len + 1
     result = []
     for index in range(len(data) - sequence_length):
@@ -39,14 +40,17 @@ def load_data(stock, seq_len):
     row = round(0.9 * result.shape[0])
     train = result[:int(row), :]
     x_train = train[:, :-1]
-    y_train = train[:, -1][:,-1]
+    y_train = train[:, -1][:, -1]
     x_test = result[int(row):, :-1]
-    y_test = result[int(row):, -1][:,-1]
+    y_test = result[int(row):, -1][:, -1]
 
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], amount_of_features))
-    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], amount_of_features))  
+    x_train = np.reshape(
+        x_train, (x_train.shape[0], x_train.shape[1], amount_of_features))
+    x_test = np.reshape(
+        x_test, (x_test.shape[0], x_test.shape[1], amount_of_features))
 
     return [x_train, y_train, x_test, y_test]
+
 
 def build_model(layers):
     model = Sequential()
@@ -67,27 +71,30 @@ def build_model(layers):
     model.add(Activation("linear"))
 
     start = time.time()
-    model.compile(loss="mse", optimizer="rmsprop",metrics=['accuracy'])
+    model.compile(loss="mse", optimizer="rmsprop", metrics=['accuracy'])
     print("Compilation Time : ", time.time() - start)
     return model
 
+
 def build_model2(layers):
-        d = 0.2
-        model = Sequential()
-        model.add(LSTM(128, input_shape=(layers[1], layers[0]), return_sequences=True))
-        model.add(Dropout(d))
-        model.add(LSTM(64, input_shape=(layers[1], layers[0]), return_sequences=False))
-        model.add(Dropout(d))
-        model.add(Dense(16,init='uniform',activation='relu'))        
-        model.add(Dense(1,init='uniform',activation='relu'))
-        model.compile(loss='mse',optimizer='adam',metrics=['accuracy'])
-        return model
+    d = 0.2
+    model = Sequential()
+    model.add(LSTM(128, input_shape=(
+        layers[1], layers[0]), return_sequences=True))
+    model.add(Dropout(d))
+    model.add(LSTM(64, input_shape=(
+        layers[1], layers[0]), return_sequences=False))
+    model.add(Dropout(d))
+    model.add(Dense(16, init='uniform', activation='relu'))
+    model.add(Dense(1, init='uniform', activation='relu'))
+    model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+    return model
 
 
 # In[10]:
 
 
-data=QA.QA_fetch_stock_day_adv('000001','2000-01-01','2018-01-31').to_qfq()
+data = QA.QA_fetch_stock_day_adv('000001', '2000-01-01', '2018-01-31').to_qfq()
 
 
 # In[14]:
@@ -99,7 +106,7 @@ data.plot()
 # In[21]:
 
 
-used_data=pd.concat([data.open,data.high,data.close],axis=1)
+used_data = pd.concat([data.open, data.high, data.close], axis=1)
 
 
 # In[25]:
@@ -118,7 +125,7 @@ X_train
 # In[28]:
 
 
-model = build_model2([3,window,1])
+model = build_model2([3, window, 1])
 
 
 # In[29]:
@@ -137,13 +144,13 @@ model.fit(
 
 
 # print(X_test[-1])
-diff=[]
-ratio=[]
+diff = []
+ratio = []
 p = model.predict(X_test)
 for u in range(len(y_test)):
     pr = p[u][0]
     ratio.append((y_test[u]/pr)-1)
-    diff.append(abs(y_test[u]- pr))
+    diff.append(abs(y_test[u] - pr))
     #print(u, y_test[u], pr, (y_test[u]/pr)-1, abs(y_test[u]- pr))
 
 
@@ -152,8 +159,7 @@ for u in range(len(y_test)):
 
 import matplotlib.pyplot as plt2
 
-plt2.plot(p,color='red', label='prediction')
-plt2.plot(y_test,color='blue', label='y_test')
+plt2.plot(p, color='red', label='prediction')
+plt2.plot(y_test, color='blue', label='y_test')
 plt2.legend(loc='upper left')
 plt2.show()
-
